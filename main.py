ea69ced7f23a1ec32613802c01dc1f639fd3554a
ulukbekov05 <samaganuluukvekov@gmail.com>
2024-12-16 21:13:51 +0600

    kda dskf sak dskfjs

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+name=int(input('Томонку диапазон: '))
+name1=int(input('Жогорку диапазон: '))
+
+
+num = random.randint(name,name1)
+men=0
+while True:
+    men+=1
+    sem=int(input(f'{name} жана {name1} ортосундагы санды табыныз: '))
+    if sem > num:
+     print('Сиз жазган сан чон')
+    elif sem < num:
+     print('Сиз жазган сан кичине')
+    else:
+        print(f'Куттуктайбыз сиз {sem} санын {men} иретте таптыныз ')
+        break
